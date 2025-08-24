853677a80ed58c17a363e66552337602e065438a
José Sánchez-Gallego <gallegoj@uw.edu>
2025-08-24 14:41:00 -0700

    Release 1.2.7

--- sep.pyx
+++ sep.pyx
@@ -15,7 +15,7 @@ from cpython.version cimport PY_MAJOR_VERSION
 
 np.import_array()  # To access the numpy C-API.
 
-__version__ = "1.2.6"
+__version__ = "1.2.7"
 
 # -----------------------------------------------------------------------------
 # Definitions from the SEP C library
